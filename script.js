1607e0f2c29c0819583076d5c6e75b54f4b1c8c3
TheCosmomancer <cosmomancer@proton.me>
2026-09-22 23:57:14 +0330

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let me_desc = [
+    'Developer',
+    'NixOS Cultist',
+    'Ball Wizard',
+];
+document.getElementById('me-desc').textContent = me_desc[Math.floor(Math.random() * me_desc.length)];
